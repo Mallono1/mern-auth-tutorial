1d6ae78b12b2905c24a4f8b4456f298956e36a1b
Mallono1 <mlazik1@gmail.com>
2025-03-19 21:39:29 +0100

    first parts of the server

---
+++ server/server.js
@@ -0,0 +1,21 @@
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import cookieParser from "cookie-parser";
+import { connect } from "mongoose";
+import connectDB from "./config/mongodb.js";
+import authRouter from "./routes/auth.js";
+
+const app = express();
+const port = process.env.PORT || 5000; // default port to listen
+connectDB();
+
+app.use(express.json());
+app.use(cors({ credentials: true }));
+app.use(cookieParser());
+
+//API Endpoints
+app.get("/", (req, res) => res.send("Hello World!"));
+app.use("/api/auth", authRouter);
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
